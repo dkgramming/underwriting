dcf32144404159c8104413622e07c226112c4550
David Kisluk <david.kisluk@gmail.com>
2018-03-16 11:30:32 -0400

    Update Unit and Property specs

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:units).dependent(:destroy) }
+
+  it { should validate_presence_of(:address) }
+
+  it do 
+    should validate_numericality_of(:marketing_expense).
+      is_greater_than_or_equal_to(0)
+  end
+  
+  it do 
+    should validate_numericality_of(:tax_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:insurance_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:repair_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:administration_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:payroll_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:utility_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:management_expense).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it { should validate_presence_of(:cap_rate) }
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:property) }
+  it { should validate_presence_of(:number) }
+  it { should have_db_index([:number, :property]).unique(:true) }
+
+  it do 
+    should validate_numericality_of(:monthly_rent).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:bedroom_count).
+      is_greater_than_or_equal_to(0)
+  end
+
+  it do 
+    should validate_numericality_of(:bathroom_count).
+      is_greater_than_or_equal_to(0)
+  end
 end
